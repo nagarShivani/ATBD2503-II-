87e017c6825e90ef520ed7d33b370967788da362
Aman Singh <166361363+AmanSingh0710@users.noreply.github.com>
2025-04-23 09:05:56 +0530

    delete Employees.js

--- Employees.js
+++ Employees.js
@@ -1,8 +1,1 @@
-const employees = [
-    { id: 1, name: "Aman singh", designation: "Software Engineer", department: "IT", salary: 75000 },
-    { id: 2, name: "Sachin singh", designation: "Project Manager", department: "Management", salary: 90000 },
-    { id: 3, name: "Krishna singh", designation: "HR Executive", department: "HR", salary: 65000 },
-    { id: 4, name: "Amar singh", designation: "UI/UX Designer", department: "Design", salary: 72000 },
-];
 
-module.exports = employees;

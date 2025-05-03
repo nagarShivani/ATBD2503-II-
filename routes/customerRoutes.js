42856eee4a0855bbd9244fc85992118a7c788def
Aman Singh <166361363+AmanSingh0710@users.noreply.github.com>
2025-05-03 17:19:43 +0530

    Update customerRoutes.js

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Customer = require('../models/Customer');
+const customerController = require('../controllers/customerController');
+
 
 // Register Customer
-router.post('/register', async (req, res) => {
-  try {
-    const customer = new Customer(req.body);
-    await customer.save();
-    res.status(201).json({ message: "Customer registered successfully", customer });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/register', customerController.registerCustomer);
 
 // Get All Customers
-router.get('/all', async (req, res) => {
-  try {
-    const customers = await Customer.find().populate('userId', 'name email');
-    res.json(customers);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get('/all', customerController.getAllCustomers);
 
 // Get Single Customer
-router.get('/:id', async (req, res) => {
-  try {
-    const customer = await Customer.findById(req.params.id).populate('userId', 'name email');
-    if (!customer) {
-      return res.status(404).json({ message: 'Customer not found' });
-    }
-    res.json(customer);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get('/:id', customerController.getSingleCustomer);
 
 module.exports = router;
